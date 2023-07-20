72e97d187f86758719deb2a0319452e8f943ea66
Elrico Landrew <dirkieys21@gmail.com>
2023-07-20 20:30:35 +0200

    Adding JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict'
+const btnSubmit = document.querySelector('.subscribe');
+const email = document.querySelector('.email');
+const errorText = document.querySelector('.error-text');
+const inputField = document.querySelector('input-field')
+
+const validEmail = function (inputEmail) {
+    let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+    return regex.test(inputEmail);
+  };
+  
+  const validate = function (input, message) {
+      // console.log(message);
+      input.parentNode.classList.add("error"); 
+      if (!validEmail(email.value)) {
+        email.parentNode.classList.add("error");
+        errorText.textContent = `${message}`
+      } else {
+        success(input);
+      }
+      
+  };
+  const success = function (input) {
+    input.parentNode.classList.remove("error");
+    errorText.textContent = '';
+  }
+btnSubmit.addEventListener('click', function(e){
+    e.preventDefault();
+    validate(email, "Please enter a vaild email address...!")
+})
